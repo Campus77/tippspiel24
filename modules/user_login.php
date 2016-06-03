e77d60ffa94adcb2f47a6016f71c91a35018b064
Campus77 <marcel@daneyko.org>
2016-06-03 10:59:19 +0200

    added flags fixed file header

--- modules/user_login.php
+++ modules/user_login.php
@@ -1,4 +1,5 @@
-<!--
+<?php
+/*
 This file is part of tippspiel24.
 
 tippspiel24 is free software: you can redistribute it and/or modify
@@ -13,50 +14,52 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with tippspiel24.  If not, see <http://www.gnu.org/licenses/>.
- -->
- <?php
-session_start();
+*/
 if (isset($_POST["username"]) and isset($_POST["password"]))
 {
 	$user = $_POST["username"];
 	$pwd = md5($_POST["password"]);
 
-	$ini_array = parse_ini_file("../config.ini", TRUE);
+	$ini_array = parse_ini_file("../config.ini", TRUE);	
 	$mySql = new mysqli($ini_array["database"]["host"], $ini_array["database"]["user"], $ini_array["database"]["pwd"], $ini_array["database"]["db"]);
 
 	/* check connection */
-	if (mysqli_connect_errno()) {
-		printf("Connect failed: %s\n", mysqli_connect_error());
-		session_destroy();
-		exit();
+	if ($mySql->connect_error) {
+		echo('Connect Error (' . $mySql->connect_errno . ') ' . $mySql->connect_error);
+		exit;
 	}
 
 	$sql="SELECT ID, Password FROM competition_users WHERE Username='$user';";
-	$result = mysqli_query($mySql, $sql);
+	$result = $mySql->query($sql);
 	if (!$result) {
 	  die('Error: ' . mysqli_error($mySql));
 	  session_destroy();
-	}
+	  exit;
+	}	
 
 	$row = mysqli_fetch_assoc($result);
 	$mySql->close();
 	
 	if ($row['Password'] == $pwd)
 	{
+	   session_start();
+		
 	   $_SESSION["activeUser"] = $user;
 	   $_SESSION["activeUserId"] = $row['ID'];
-		header('Location: ../ranking.php');
+	   //error_reporting(E_ALL | E_WARNING | E_NOTICE);
+	   //ini_set('display_errors', TRUE);
+		
+	   header('Location: ../ranking', true, 301);
+	   exit;
 	}
 	else
 	{
-		header('Location: ../index.php');
-		session_destroy();
+		header('Location: /', true, 301);
+		exit;
 	}
 }
 else
 {
    exit("Variablen nicht gesetzt!");
-   session_destroy();
 }
 ?>
-
